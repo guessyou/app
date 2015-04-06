f3ac8baa3644684cfa6749fc49ca400b8cdaebaa
cloudyan <cloudyan@qq.com>
2015-04-06 12:23:35 +0800

    开发 标记完成/删除todo项功能

--- todomvc/backbone/js/views/todo-view.js
+++ todomvc/backbone/js/views/todo-view.js
@@ -10,7 +10,9 @@ app.TodoView = Backbone.View.extend({
     template: _.template( $('#item-template').html() ),
 
     events: {
+        'click .toggle': 'toggleCompleted',
         'dbclick label': 'edit',
+        'click .destroy': 'clear',
         'keypress .edit': 'updateOnEnter',
         'blur .edit': 'close'
     },
@@ -19,12 +21,35 @@ app.TodoView = Backbone.View.extend({
         //监听Todo模型的change事件，todo更新时，应用程序会重新渲染试图
         //注意：Todo模型是通过AppView在arguments参数传递进来的，并且在this.model上自动可用
         this.listenTo(this.model, 'change', this.render);
+
+        this.listenTo(this.model, 'destroy', this.remove);
+        this.listenTo(this.model, 'visible', this.toggleVisible);
     },
 
     render: function(){
         this.$el.html( this.template( this.model.toJSON() ) );
+
+        this.$el.toggleClass( 'completed', this.model.get('completed') );
+        this.toggleVisible();
+
         this.$input = this.$('.edit');
         return this;
+    },
+
+    toggleVisible: function(){
+        this.$el.toggleClass( 'hidden', this.isHidden() )
+    },
+
+    isHidden: function(){
+        var isCompleted = this.model.get('completed');
+        return (
+            (!isCompleted && app.TodoFilter === 'completed')
+            || (isCompleted && app.TodoFilter === 'active')
+        );
+    },
+
+    toggleCompleted: function(){
+        this.model.toggle();
     },
 
     edit: function(){
@@ -37,6 +62,8 @@ app.TodoView = Backbone.View.extend({
 
         if(value){
             this.model.save({title: value});
+        }else{
+            this.clear();
         }
 
         this.$el.removeClass('editing');
@@ -46,6 +73,10 @@ app.TodoView = Backbone.View.extend({
         if( e.which === ENTER_KEY ){
             this.close();
         }
-    }
+    },
+
+    clear: function(){
+        this.model.destroy();
+    },
 
 });
