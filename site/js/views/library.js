c6ce95d60371c1ebffa6498c88286b6192a4eeac
cloudyan <cloudyan@qq.com>
2015-04-13 11:42:01 +0800

    新增添加模型

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -1,14 +1,18 @@
 
 var app = app || {};
 
-
-
 app.LibraryView = Backbone.View.extend({
     el: '#books',
+
+    events: {
+        'click #add': 'addBook'
+    },
 
     initialize: function(initialBooks){
         this.collection = new app.Library( initialBooks );
         this.render();
+
+        this.listenTo(this.collection, 'add', this.renderBook );
     },
 
     render: function(){
@@ -22,5 +26,20 @@ app.LibraryView = Backbone.View.extend({
             model: item
         });
         this.$el.append( bookView.render().el );
+    },
+
+    addBook: function(e){
+        e.preventDefault();
+
+        var formData = {};
+
+        $('#addBook>div').find('input').each(function(i, el){
+            if( $(el).val() != '' ){
+                formData[el.id] = $(el).val();
+            }
+        });
+
+        this.collection.add( new app.Book(formData) );
     }
+
 });
