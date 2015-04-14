c91b202d22f9d5aa98f7f48c71f6a4abf9711f91
cloudyan <cloudyan@qq.com>
2015-04-14 12:32:39 +0800

    创建restful路由，支持4个HTTP动词

--- routes/web_router.js
+++ routes/web_router.js
@@ -14,8 +14,19 @@ var Book = new mongoose.Schema({
     author: String,
     releaseDate: Date
 });
+
+
 var BookModel = mongoose.model( 'Book', Book );
 
+
+var error = function(err){
+    return function(){
+        if(err){
+            console.log(err);
+            return;
+        }
+    }
+}
 // var config = require('../config'),
 //     crypto = require('crypto'),
 //     passport = require('passport');
@@ -24,23 +35,127 @@ router.get('/api', function(req, res){
     res.send('Api is running');
 });
 
+
 router.get('/api/books', function(req, res){
     return BookModel.find(function(err,books){
-        if(!err){
-            return res.send(books);
-        }else{
-            return console.log(err);
-        }
+        error(err);
+        res.send(books);
     })
 });
 
-/*
+/* test
 jQuery.get('/api/books/',function(data, textStatus, jqXHR){
-    console.log('Get response');
+    console.log('Get response:');
     console.dir(data);
     console.log(textStatus);
     console.log(jqXHR);
 })
+**/
+
+
+router.post('/api/books', function(req, res){
+    var book = new BookModel({
+        title: req.body.title,
+        author: req.body.author,
+        releaseDate: req.body.releaseDate
+    });
+
+    book.save(function(err){
+        error(err);
+        console.log('created');
+        
+    });
+    return res.send(book);
+});
+
+/* test
+jQuery.post('/api/books/', {
+        'title': 'Javascript',
+        'author': 'Douglas',
+        'releaseDate': new Date(2008,4,1).getTime()
+    }, function(data, textStatus, jqXHR){
+    console.log('Post response:');
+    console.dir(data);
+    console.log(textStatus);
+    console.log(jqXHR);
+})
+**/
+
+
+router.get('/api/books/:id', function(req, res){
+    var params = req.params.id;
+    return BookModel.findById(params, function(err,book){
+        error(err);
+        res.send(book);
+    })
+});
+/* test
+jQuery.get('/api/books/552c86d4a94bf75016810aea',function(data, textStatus, jqXHR){
+    console.log('Get response:');
+    console.dir(data);
+    console.log(textStatus);
+    console.log(jqXHR);
+})
+**/
+
+
+router.put('/api/books/:id', function(req, res){
+    console.log('    Updating Book ' + req.body.title);
+    var params = req.params.id;
+    return BookModel.findById(params, function(err,book){
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.releaseDate = req.body.releaseDate;
+
+        return book.save(function(err){
+            error(err);
+            console.log('book updated');
+            return res.send(book);
+        })
+        
+    })
+});
+/* test
+jQuery.ajax({
+    url: '/api/books/552c86d4a94bf75016810aea',
+    type: 'PUT',
+    data: {
+        'title': 'Javascript 111',
+        'author': 'Douglas',
+        'releaseDate': new Date(2008,4,1).getTime()
+    },
+    success: function(data, textStatus, jqXHR){
+        console.log('Post response:');
+        console.dir(data);
+        console.log(textStatus);
+        console.log(jqXHR);
+    }
+});
+**/
+
+
+router.delete('/api/books/:id', function(req, res){
+    console.log('    Deleting Book with id: ' + req.params.id);
+    var params = req.params.id;
+    return BookModel.findById(params, function(err,book){
+        return book.remove(function(err){
+            error(err);
+            console.log('Book removed');
+            return res.send('');
+        })
+    })
+});
+/* test
+jQuery.ajax({
+    url: '/api/books/552c86d4a94bf75016810aea',
+    type: 'DELETE',
+    success: function(data, textStatus, jqXHR){
+        console.log('Post response:');
+        console.dir(data);
+        console.log(textStatus);
+        console.log(jqXHR);
+    }
+});
 **/
 
 // router.get('/help', staticController.help);
